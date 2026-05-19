3b15785b59726cf5eabaebfa61b13ac537576892
fakehesapyanhsp-cmyk <fake.hesap.yanhsp@gmail.com>
2026-05-19 20:51:32 +0300

    Add: design-hashmap - submission-1

---
+++ Data Structures & Algorithms/design-hashmap/submission-1.cpp
@@ -0,0 +1,42 @@
+class MyHashMap {
+public:
+    vector<pair<int ,int>> data;
+    MyHashMap() {
+        
+    }
+    
+    void put(int key, int value) {
+        for (int i = 0; i < data.size(); i++) {
+            if (data[i].first == key) {
+                data[i].second = value;
+                return;
+            }
+        }
+        data.push_back({key, value});
+    }
+    
+    int get(int key) {
+        for (int i = 0; i < data.size(); i++) {
+            if (data[i].first == key) {
+                return data[i].second;
+            }
+        }
+        return -1;
+    }
+    
+    void remove(int key) {
+        for (int i = 0; i < data.size(); i++) {
+            if (data[i].first == key) {
+                data.erase(data.begin() + i);
+            }
+        }
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
