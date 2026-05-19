b05c5c988e8cdd6f375e13a1ded7f5827e566169
fakehesapyanhsp-cmyk <fake.hesap.yanhsp@gmail.com>
2026-05-19 21:07:12 +0300

    Add: sort-an-array - submission-17

---
+++ Data Structures & Algorithms/sort-an-array/submission-17.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    void merge(vector<int>& nums, int l, int m, int r) {
+        vector<int> buffer(r-l+1, 0);
+        int n1 = m-l+1;
+        int n2 = r-m;
+        int p1 = 0;
+        int p2 = m-l+1;
+        int p3 = l;
+        for (int i = 0; i < n1; i++) {
+            buffer[i] = nums[i+l];
+        }
+        for (int i = 0; i < n2; i++) {
+            buffer[i+p2] = nums[m+1+i];
+        }
+        while (p1 < n1 && p2 < n2+n1) {
+            if (buffer[p1] < buffer[p2]) {
+                nums[p3++] = buffer[p1++];
+            } else {
+                nums[p3++] = buffer[p2++];
+            }
+        }
+        while (p2 < n2+n1) {
+            nums[p3++] = buffer[p2++];
+        }
+        while (p1 < n1) {
+            nums[p3++] = buffer[p1++];
+        }
+    }
+
+    void mergeSort(vector<int>& nums, int l, int r) {
+        if (l == r) {
+            return;
+        }
+        int m = l + (r - l) / 2;
+        
+        mergeSort(nums, l, m);
+        mergeSort(nums, m+1, r);
+
+        merge(nums, l, m, r);
+    }
+
+    vector<int> sortArray(vector<int>& nums) {
+        mergeSort(nums, 0, nums.size()-1);
+        return nums;
+    }
+};
