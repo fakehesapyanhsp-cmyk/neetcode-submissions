8737eb560b10845d7ba233c9be5038b5db0287a5
fakehesapyanhsp-cmyk <fake.hesap.yanhsp@gmail.com>
2026-05-19 20:36:11 +0300

    Add: remove-element - submission-6

---
+++ Data Structures & Algorithms/remove-element/submission-6.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int l = 0, r = nums.size();
+        while (l < r) {
+            if (nums[l] == val) {
+                swap(nums[l], nums[--r]);
+            }
+            l++;
+        }
+        return r;
+    }
+};
