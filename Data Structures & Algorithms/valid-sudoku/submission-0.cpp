09657dc6913f899eeeec9d8a43afe5e5958d8681
fakehesapyanhsp-cmyk <fake.hesap.yanhsp@gmail.com>
2026-05-21 20:58:10 +0300

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unoredered_set<int> seen;
+        for (int j = 0; j < 9; j++) {
+            for (int i = 0; i < 9; i++) {
+                if (board[j][i] == '.') continue;
+                if (seen.count(board[j][i])) {
+                    return false;
+                }
+                seen.insert(board[j][i]);
+            }
+        }
+        seen.clear();
+        for (int j = 0; j < 9; j++) {
+            for (int i = 0; i < 9; i++) {
+                if (board[i][j] == '.') continue;
+                if (seen.count(board[i][j])) {
+                    return false;
+                }
+                seen.insert(board[i][j]);
+            }
+        }
+        return true;
+    }
+};
