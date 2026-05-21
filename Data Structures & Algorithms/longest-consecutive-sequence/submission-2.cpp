fac9e4abb924be345b17c29e366e341b28aa3be0
fakehesapyanhsp-cmyk <fake.hesap.yanhsp@gmail.com>
2026-05-21 21:12:17 +0300

    Add: longest-consecutive-sequence - submission-2

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-2.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> seen;
+        for (auto num : nums) {
+            seen.insert(num);
+        }
+        for (auto num : seen) {
+            int count = 1;
+            int target = num + 1;
+            while (seen.count(target)) {
+                count++;
+                target++;
+            }
+            ans = max(ans, count);
+        }
+        return ans;
+    }
+};
