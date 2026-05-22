0fe4af1cff51d60dce46e21f70767576942f00d4
fakehesapyanhsp-cmyk <fake.hesap.yanhsp@gmail.com>
2026-05-22 06:51:31 +0300

    Add: majority-element-ii - submission-1

---
+++ Data Structures & Algorithms/majority-element-ii/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int k = nums.size()/3;
+        unordered_map<int, int> count;
+        for (auto num : nums) {
+            count[num]++;
+        }
+        vector<int> ans;
+        for (auto [k, v] : count) {
+            if (v >= k) {
+                ans.push_back(k);
+            }
+        }
+        return ans;
+    }
+};
