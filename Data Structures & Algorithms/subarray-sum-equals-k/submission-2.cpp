614403ed05fb9d648c40752b0d84953129a610c2
fakehesapyanhsp-cmyk <fake.hesap.yanhsp@gmail.com>
2026-05-22 07:11:50 +0300

    Add: subarray-sum-equals-k - submission-2

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+        unordered_map<int, vector<int>> prefixSum;
+        prefixSum[0].push_back(0);
+        int prefix=0;
+        int ans=0;
+        for (int i = 0; i < n; i++) {
+            prefix+=nums[i];
+            if (prefixSum.count(prefix-k)) {
+                ans+=prefixSum[prefix-k].size();
+            }
+            prefixSum[prefix].push_back(prefix);
+        }
+        return ans;
+    }
+};
