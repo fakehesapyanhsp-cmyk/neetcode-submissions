4474986da2a6e1b21ef4884d11b83ab0442aa601
fakehesapyanhsp-cmyk <fake.hesap.yanhsp@gmail.com>
2026-05-23 15:41:52 +0300

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int n = s.size();
+        int l = 0, r = n - 1;
+        while (l < r) {
+            while (l < r && s[l] == ' ') l++;
+            while (l < r && s[r] == ' ') r--;
+            if (l >= r) break;
+            char lv = s[l];
+            if (lv >= 'A' && lv <= 'Z') lv += 'a' - 'A';
+            char rv = s[r];
+            if (rv >= 'A' && rv <= 'Z') rv += 'a' - 'A';
+            if (lv != rv) return false;
+            r--;
+            l++;
+        }
+        return true;
+    }
+};
