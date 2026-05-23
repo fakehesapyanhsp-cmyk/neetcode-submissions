5bcfbab589ceb54338a71c54493c3849bdaeb357
fakehesapyanhsp-cmyk <fake.hesap.yanhsp@gmail.com>
2026-05-23 15:47:08 +0300

    Add: valid-palindrome-ii - submission-1

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool validPalindrome(string s) {
+        int n = s.size();
+        int l = 0, r = n - 1;
+        int c = 0;
+        while (l < r) {
+            if (s[l] != s[r]) {
+                c++;
+                if (c > 1) {
+                    return false;
+                }
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+};
