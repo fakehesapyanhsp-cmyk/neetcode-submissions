5b8e0d308f8794e78f236af1469091d75c058d3a
fakehesapyanhsp-cmyk <fake.hesap.yanhsp@gmail.com>
2026-05-23 16:12:27 +0300

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int n = numbers.size();
+        int l = 0, r = n-1;
+        for (;;) {
+            int val = numbers[l] + numbers[r];
+            if (val == target) return {l, r};
+            if (val < target) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+    }
+};
