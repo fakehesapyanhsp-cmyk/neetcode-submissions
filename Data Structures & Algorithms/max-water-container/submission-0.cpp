eaaf8435b97ec55520f1cfb402245ee00f4e1cd0
fakehesapyanhsp-cmyk <fake.hesap.yanhsp@gmail.com>
2026-05-24 17:54:22 +0300

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxArea(vector<int>& heights) {
+        int ans = 0;
+        int n = heights.size();
+        int l = 0, r = n-1;
+        while (l < r) {
+            ans = max(ans, r - l * min(heights[l], heights[r]));
+            if (height[l] < height[r]) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        return ans;
+    }
+};
