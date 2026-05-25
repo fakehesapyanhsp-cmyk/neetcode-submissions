7ff666c2351e25a5427b62d5f9963984529703cb
fakehesapyanhsp-cmyk <fake.hesap.yanhsp@gmail.com>
2026-05-25 22:21:30 +0300

    Add: boats-to-save-people - submission-2

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-2.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(), people.end());
+        int n = people.size();
+        int l = 0, r = n-1;
+        int ans = 0;
+        while (l < r) {
+            if (l + r <= limit) {
+                l++;
+            }
+            ans++;
+            r--;
+        }
+        return ans;
+    }
+};
