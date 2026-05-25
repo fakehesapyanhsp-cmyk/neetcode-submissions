290a78d5de825d1d1cbdda1beac785a7cdaac4a2
fakehesapyanhsp-cmyk <fake.hesap.yanhsp@gmail.com>
2026-05-25 22:36:58 +0300

    Add: trapping-rain-water - submission-3

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-3.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int ans = 0;
+        int l = 0, r = n-1;
+        int leftMax = height[l], rightMax = height[r];
+        while (l < r) {
+            if (leftMax > rightMax) {
+                r--;
+                rightMax = max(height[r], rightMax);
+                ans += rightMax - height[r];
+            } else {
+                l++;
+                leftMax = max(leftMax, height[l]);
+                ans += leftMax - height[l];
+            }
+        }
+        return ans;
+    }
+};
