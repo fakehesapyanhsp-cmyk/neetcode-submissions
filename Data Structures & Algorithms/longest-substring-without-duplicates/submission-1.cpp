7c0d4f6509d3bab0d067bc7e60b7457f892751d0
fakehesapyanhsp-cmyk <fake.hesap.yanhsp@gmail.com>
2026-05-26 11:38:11 +0300

    Add: longest-substring-without-duplicates - submission-1

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char, int> indexes;
+        int l = 0;
+        int ans = 0;
+        int n = s.size();
+        int r;
+        for (r = 0; r < n; r++) {
+            if (indexes.count(s[r])) {
+                ans = max(r - l);
+                l = indexes[s[r]]+1;
+            }
+            indexes[s[r]] = r;
+        }
+        ans = max(ans, r - l);
+        return ans;
+    }
+};
