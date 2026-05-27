fc9fc9783865b9588fa0f0f4cebc71952b955752
fakehesapyanhsp-cmyk <fake.hesap.yanhsp@gmail.com>
2026-05-27 17:01:22 +0300

    Add: permutation-string - submission-10

---
+++ Data Structures & Algorithms/permutation-string/submission-10.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        array<int, 26> counter1{0};
+        array<int, 26> counter2{0};
+        for (auto c : s1) {
+            counter1[c-'a']++;
+        }   
+        int l = 0;
+        int n2 = s2.size();
+        int n1 = s1.size();
+        int matches = 0;
+        for (int r = 0; r < n2; r++) {
+            int ri = s2[r] - 'a';
+            if (counter1[ri] == 0) {
+                while (l <= r) {
+                    int li = s2[l] - 'a';
+                    if (counter1[li]) {
+                        matches--;
+                    }
+                    counter2[li]--;
+                    l++;
+                }
+            } else {
+                counter1[ri]++;
+                matches++;
+                while (counter1[ri] < counter2[ri]) {
+                    int li = s2[l] - 'a';
+                    if (counter1[li]) {
+                        matches--;
+                    }
+                    counter2[li]--;
+                    l++;
+                }
+                if (matches == n1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
