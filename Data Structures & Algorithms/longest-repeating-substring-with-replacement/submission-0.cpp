abca98ec0ff1ff40e7bde6cb5f70734414e2f53e
fakehesapyanhsp-cmyk <fake.hesap.yanhsp@gmail.com>
2026-05-27 16:38:29 +0300

    Add: longest-repeating-substring-with-replacement - submission-0

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        unordered_map<int, int> counter;
+        int l = 0;
+        int n = s.size();
+        int maxf = 0;
+        int ans = 0;
+        for (int r = 0; r < n; r++) {
+            counter[s[r]]++;
+            maxf = max(counter[s[r]], maxf);
+            while (r-l+1-maxf > k) {
+                l++;
+                counter[s[l]]--;
+            }
+            ans = max(r-l+1, ans);
+        }
+        return ans;
+    }
+};
