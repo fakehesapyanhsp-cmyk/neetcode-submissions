6811a94c2dd9a77ad5567be27f7475163d066630
fakehesapyanhsp-cmyk <fake.hesap.yanhsp@gmail.com>
2026-05-29 15:43:58 +0300

    Add: find-k-closest-elements - submission-2

---
+++ Data Structures & Algorithms/find-k-closest-elements/submission-2.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int count = 0;
+        int current = 0;
+        int l, r;
+        int n = arr.size();
+        while (l < r) {
+            int p = l + (r - l) / 2;
+            if (arr[p] == x) {
+                l = p - 1;
+                r = p + 1;
+                count++;
+                break;
+            } else if (arr[p] > x) {
+                r = p - 1;
+            } else {
+                l = p;
+            }
+        }
+        cout << l << r;
+        while (count < k && l >= 0 && r < n) {
+            if (abs(arr[l]-x) < abs(arr[r]-x)) {
+                l--;
+                count++;
+            } else {
+                r++;
+                count++;
+            }
+        }
+        while (count < k && l >= 0) {
+            l--;
+            count++;
+        }
+        while (count < k && r < n) {
+            r++;
+            count++;
+        }
+        vector<int> ans;
+        for (int i = l + 1; i < r; i++) {
+            ans.push_back(arr[i]);
+        }
+        return ans;
+    }
+};
